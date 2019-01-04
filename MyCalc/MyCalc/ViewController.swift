e8c4aca53d1f904f85d9a2ae795357497749336b
yokubo <utsue.38@gmail.com>
2019-01-04 23:12:44 +0900

    implement tap buttons logic

--- MyCalc/MyCalc/ViewController.swift
+++ MyCalc/MyCalc/ViewController.swift
@@ -10,11 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var priceField: UITextField!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func tapNumberButton(_ sender: UIButton) {
+        var price: Int
+        
+        if let currentPriceString = priceField.text {
+            if let currentPriceInt = Int(currentPriceString) {
+                switch sender.currentTitle {
+                case "00": price = currentPriceInt * 100
+                case "0": price = currentPriceInt * 10
+                case "1": price = currentPriceInt * 10 + 1
+                case "2": price = currentPriceInt * 10 + 2
+                case "3": price = currentPriceInt * 10 + 3
+                case "4": price = currentPriceInt * 10 + 4
+                case "5": price = currentPriceInt * 10 + 5
+                case "6": price = currentPriceInt * 10 + 6
+                case "7": price = currentPriceInt * 10 + 7
+                case "8": price = currentPriceInt * 10 + 8
+                case "9": price = currentPriceInt * 10 + 9
+                default: price = currentPriceInt
+                }
+            } else {
+                price = 0
+            }
+        } else {
+            price = 0
+        }
 
+        priceField.text = "\(price)"
+    }
+
+    @IBAction func tapClearButton(_ sender: UIButton) {
+        priceField.text = "0"
+    }
 }
-
